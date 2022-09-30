74145854340ed206fb09b722e4a78acf0717a264
Savenok <Romasav2017@gmail.com>
2022-09-30 19:18:42 +0300

    67

--- Program.cs
+++ Program.cs
@@ -1,54 +1,20 @@
-﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
-// m = 2, n = 3 -> A(m,n) = 9
-// m = 3, n = 2 -> A(m,n) = 29
-
-
-// Первый вариант решения по формуле 
-// (m,n,0)=m+n
-// (m,n,1)=m*n
-// (m,n,2)=m^{n}
-
-// Console.Clear();
-// Console.Write("Введите число M: ");
-// int numberM = int.Parse(Console.ReadLine());
-// Console.Write("Введите число N: ");
-// int numberN = int.Parse(Console.ReadLine());
-// Console.Write("Введите число x: ");
-// int numberx = int.Parse(Console.ReadLine());
-// void AckermannFunction(int m, int n, int x)
-// {
-//     int result = 0;
-//     if ((m < 0) || (n < 0))
-//     {
-//         Console.WriteLine("Ошибка: Одно из чисел меньше нуля");
-//         Console.Write("Введите число M: ");
-//         int numberM2 = int.Parse(Console.ReadLine());
-//         Console.Write("Введите число N: ");
-//         int numberN2 = int.Parse(Console.ReadLine());
-//         Console.Write("Введите число x: ");
-//         int numberx2 = int.Parse(Console.ReadLine());
-//         AckermannFunction(numberM2, numberN2, x);
-//     }
-//     else
-//     {
-//         if (x == 0) result = m + n;
-//         else if (x == 1) result = m*n;
-//         else if (x == 2) result = Convert.ToInt32(Math.Pow(m , n));
-//     }
-//     Console.WriteLine($" m = {m}, n = {n} -> F(m,n,{x}) = {result}");
-// }
-// AckermannFunction(numberM, numberN, numberx);
-
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
 Console.Clear();
-Console.Write("Введите число M: ");
-int numberM = Math.Abs(int.Parse(Console.ReadLine()));
-Console.Write("Введите число N: ");
-int numberN = Math.Abs(int.Parse(Console.ReadLine()));
-int AckermannFunction(int m, int n)
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int SumNumbersInNumber(int n)
 {
-    if (m == 0) return n + 1;
-    else if (n == 0) return AckermannFunction(m - 1, 1);
-    else return AckermannFunction(m - 1, AckermannFunction(m, n - 1));
+    int oldn = n;
+    int sum = 0;
+    while (n > 0)
+    {
+        int g = n % 10;
+        n = n / 10;
+        sum = sum + g;
+    }
+    Console.WriteLine($"{oldn} - > {sum}");
+    return sum;
 }
-int res = AckermannFunction(numberM, numberN);
-Console.WriteLine(res);
+SumNumbersInNumber(number);
